1aa94fdb3d41d388c6d35d17ef2f307f6bdae7bf
Abdullahkhan80 <abdullahabbdullah8@gmail.com>
2025-06-26 22:01:13 +0500

    make some changes in project section

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -12,21 +12,21 @@ const Projects = () => {
         id: 1,
         title: "White Space Website",
         description: "A powerful project management platform designed to help teams collaborate, plan, and manage daily tasks",
-        image: "../public/projects/project1.png",
+        image: "/projects/project1.png",
         link: "https://white-space-psi.vercel.app/"
       },
       {
         id: 2,
         title: "Ai consultant website",
         description: "A webiste which provide expert AI consulting services to help businesses automate processes, enhance decision-making, and unlock data-driven growth. From strategy to deployment — we turn your AI vision into reality.",
-        image: "../public/projects/project2.png",
+        image: "/projects/project2.png",
         link: "https://project2.com"
       },
       {
         id: 3,
         title: "Golf website",
         description: "A premium golf website offering everything from course details and memberships to event booking and training resources. Designed for golf enthusiasts, players, and clubs to connect and thrive.",
-        image: "../public/projects/project3.png",
+        image: "/projects/project3.png",
         link: "https://project3.com"
       
       },
@@ -34,7 +34,7 @@ const Projects = () => {
         id: 4,
         title: "burger website",
         description: "This burger website is your go-to destination for delicious, handcrafted burgers made with the freshest ingredients.",
-        image: "../public/projects/project4.png",
+        image: "/projects/project4.png",
         link: "https://project3.com"
       
       },
@@ -42,7 +42,7 @@ const Projects = () => {
         id: 5,
         title: "Real state website",
         description: "A modern real estate website offering property listings, virtual tours, and expert guidance for buyers, sellers, and renters. Find your dream home or investment with ease.",
-        image: "../public/projects/project5.png",
+        image: "/projects/project5.png",
         link: "https://project3.com"
       },
     ];
